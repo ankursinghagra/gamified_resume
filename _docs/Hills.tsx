a1a46b0cd43f1e0025ac8cdcfe38f501dd83b05e
Ankur Singh <ankursinghagra@gmail.com>
2024-11-20 20:51:35 +0530

    init

---
+++ _docs/Hills.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="Hills" tilewidth="80" tileheight="80" tilecount="77" columns="11">
+ <image source="../assets/backgrounds/Hills.png" width="880" height="560"/>
+</tileset>
---
+++ _docs/TreesDecorations.tsx
@@ -0,0 +1,115 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="TreesDecorations" tilewidth="350" tileheight="490" tilecount="37" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="../assets/backgrounds/Trees/1.png" width="175" height="135"/>
+ </tile>
+ <tile id="1">
+  <image source="../assets/backgrounds/Trees/2.png" width="65" height="70"/>
+ </tile>
+ <tile id="2">
+  <image source="../assets/backgrounds/Trees/3.png" width="80" height="70"/>
+ </tile>
+ <tile id="3">
+  <image source="../assets/backgrounds/Trees/4.png" width="55" height="70"/>
+ </tile>
+ <tile id="4">
+  <image source="../assets/backgrounds/Trees/5.png" width="65" height="65"/>
+ </tile>
+ <tile id="5">
+  <image source="../assets/backgrounds/Trees/6.png" width="155" height="125"/>
+ </tile>
+ <tile id="6">
+  <image source="../assets/backgrounds/Trees/7.png" width="60" height="60"/>
+ </tile>
+ <tile id="7">
+  <image source="../assets/backgrounds/Trees/8.png" width="80" height="65"/>
+ </tile>
+ <tile id="8">
+  <image source="../assets/backgrounds/Trees/9.png" width="55" height="65"/>
+ </tile>
+ <tile id="9">
+  <image source="../assets/backgrounds/Trees/10.png" width="60" height="60"/>
+ </tile>
+ <tile id="10">
+  <image source="../assets/backgrounds/Trees/11.png" width="120" height="135"/>
+ </tile>
+ <tile id="11">
+  <image source="../assets/backgrounds/Trees/12.png" width="120" height="135"/>
+ </tile>
+ <tile id="12">
+  <image source="../assets/backgrounds/Trees/13.png" width="70" height="110"/>
+ </tile>
+ <tile id="13">
+  <image source="../assets/backgrounds/Trees/14.png" width="70" height="110"/>
+ </tile>
+ <tile id="14">
+  <image source="../assets/backgrounds/Trees/15.png" width="65" height="70"/>
+ </tile>
+ <tile id="15">
+  <image source="../assets/backgrounds/Trees/16.png" width="55" height="70"/>
+ </tile>
+ <tile id="16">
+  <image source="../assets/backgrounds/Trees/17.png" width="65" height="65"/>
+ </tile>
+ <tile id="17">
+  <image source="../assets/backgrounds/Trees/18.png" width="120" height="125"/>
+ </tile>
+ <tile id="18">
+  <image source="../assets/backgrounds/Trees/19.png" width="120" height="125"/>
+ </tile>
+ <tile id="19">
+  <image source="../assets/backgrounds/Trees/20.png" width="70" height="100"/>
+ </tile>
+ <tile id="20">
+  <image source="../assets/backgrounds/Trees/21.png" width="70" height="100"/>
+ </tile>
+ <tile id="21">
+  <image source="../assets/backgrounds/Trees/22.png" width="75" height="55"/>
+ </tile>
+ <tile id="22">
+  <image source="../assets/backgrounds/Trees/23.png" width="80" height="60"/>
+ </tile>
+ <tile id="23">
+  <image source="../assets/backgrounds/Trees/24.png" width="60" height="50"/>
+ </tile>
+ <tile id="24">
+  <image source="../assets/backgrounds/Trees/25.png" width="30" height="30"/>
+ </tile>
+ <tile id="25">
+  <image source="../assets/backgrounds/Trees/26.png" width="25" height="15"/>
+ </tile>
+ <tile id="26">
+  <image source="../assets/backgrounds/Trees/27.png" width="25" height="15"/>
+ </tile>
+ <tile id="27">
+  <image source="../assets/backgrounds/Trees/28.png" width="15" height="15"/>
+ </tile>
+ <tile id="28">
+  <image source="../assets/backgrounds/Trees/29.png" width="130" height="110"/>
+ </tile>
+ <tile id="29">
+  <image source="../assets/backgrounds/Trees/30.png" width="135" height="115"/>
+ </tile>
+ <tile id="30">
+  <image source="../assets/backgrounds/Trees/31.png" width="110" height="105"/>
+ </tile>
+ <tile id="31">
+  <image source="../assets/backgrounds/Trees/32.png" width="350" height="490"/>
+ </tile>
+ <tile id="32">
+  <image source="../assets/backgrounds/Trees/33.png" width="350" height="470"/>
+ </tile>
+ <tile id="33">
+  <image source="../assets/backgrounds/Trees/34.png" width="190" height="370"/>
+ </tile>
+ <tile id="34">
+  <image source="../assets/backgrounds/Trees/35.png" width="190" height="350"/>
+ </tile>
+ <tile id="35">
+  <image source="../assets/backgrounds/Trees/log1.png" width="200" height="159"/>
+ </tile>
+ <tile id="36">
+  <image source="../assets/backgrounds/Trees/log2.png" width="200" height="152"/>
+ </tile>
+</tileset>
